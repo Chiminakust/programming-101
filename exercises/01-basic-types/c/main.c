7bff069e2d2961187cf37dc909f866053067942b
Maxime Guillemette <maxime_glmt@hotmail.ca>
2024-02-10 10:39:45 -0500

    exercise 1: added more tasks

--- exercises/01-basic-types/c/main.c
+++ exercises/01-basic-types/c/main.c
@@ -21,6 +21,11 @@
         }                                                               \
     } while (0)
 
+#define assert_near(x, expected, tolerance) \
+    do {                                    \
+        assert(((expected * (1 - tolerance)) <= x) && (x <= (expected * (1 + tolerance)))) \
+    } while (0)
+
 #define test_start() do {                               \
         printf(KBLU"----\t%s start\t\t\t----\n"KNRM, __func__); \
     } while(0)
@@ -32,6 +37,12 @@
 static int run_all_tests(void);
 static int test_addi(void);
 static int test_addf(void);
+static int test_maclaurin_sin(void);
+static int test_maclaurin_cos(void);
+static int test_fib(void);
+static int test_string_to_int(void);
+static int test_int_to_string(void);
+// TODO: exercises for boolean manipulation
 
 /*
  * 1.1 Create a function named addi that takes 2 integers and returns the
@@ -50,6 +61,32 @@ addi(int a, int b)
 float addf(float a, float b) {
     return a + b;
 }
+
+/*
+ * 1.3 Create a function that returns the power of x to y.
+ * All arguments and return types are floating point number.
+ */
+
+/*
+ * 1.4 Maclaurin series of sin(x) and cos(x) to any Nth degree (check wikipedia).
+ * Takes a float as argument and returns a float. Other arguments left as exercise.
+ * Functions must be named maclaurin_sin and maclaurin_cos.
+ */
+
+/*
+ * 1.5 Create a function called fib that returns the Nth term of the fibonacci sequence.
+ */
+
+/*
+ * 1.6 Create a function called string_to_int that converts a string to an integer value.
+ * The function takes a string as argument and returns an int.
+ */
+
+/*
+ * 1.7 Create a function called int_to_string that converts an int
+ * to its string representation. The function can modify an input argument
+ * and return an error code or return the string.
+ */
 
 int
 main(int argc, char **argv)
@@ -70,6 +107,26 @@ run_all_tests(void)
     }
 
     if (test_addf()) {
+        return -1;
+    }
+
+    if (test_maclaurin_sin()) {
+        return -1;
+    }
+
+    if (test_maclaurin_cos()) {
+        return -1;
+    }
+
+    if (test_fib()) {
+        return -1;
+    }
+
+    if (test_string_to_int()) {
+        return -1;
+    }
+
+    if (test_int_to_string()) {
         return -1;
     }
 
@@ -95,8 +152,49 @@ test_addf(void)
     test_start();
 
     assert(addf(1.3, 4.3) == 5.6);
-    assert(addf(1.4, 4.3) == 5.6);
+    assert(addf(0, 4.3) == 4.3);
 
     test_pass();
+    return 0;
+}
+
+static int
+test_maclaurin_sin(void)
+{
+
+    return 0;
+}
+
+static int
+test_maclaurin_cos(void)
+{
+
+    return 0;
+}
+
+static int
+test_fib(void)
+{
+    assert(fib(0) == 0);
+    assert(fib(1) == 1);
+    assert(fib(2) == 1);
+    assert(fib(3) == 2);
+    assert(fib(4) == 3);
+    assert(fib(5) == 5);
+    assert(fib(6) == 8);
+    assert(fib(19) == 4181);
+    assert(fib(-1) == 0);
+    return 0;
+}
+
+static int
+test_string_to_int(void)
+{
+    return 0;
+}
+
+static int
+test_int_to_string(void)
+{
     return 0;
 }
